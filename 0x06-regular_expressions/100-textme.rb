60e6c530ff1d7981857e69729c5c25a66485d22e
Roberto <robpalacios1@hotmail.com>
2020-07-06 19:19:42 -0500

    fix code Texme file inside emacs

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -5,4 +5,4 @@
 # The receiver phone number or name (including country code if present)
 # The flags that were used
 
-puts ARGV[0].scan(/(?<=from:|to:|flags:)[^\]]*/).join
+puts ARGV[0].scan(/(?<=from:|to:|flags:)[^\]]*/).join(',')
