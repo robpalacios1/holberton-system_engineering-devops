68a8eebfecc2051e80e68d0c7615b8ce38d3ce1f
Roberto <robpalacios1@hotmail.com>
2020-09-17 13:12:24 -0500

    indent code

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -44,13 +44,11 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
                 else:
                     instances[word] += times
 
-                    if after is None:
-                        if len(instances) == 0:
-                            print("")
-                            return
-                        instances = sorted(instances.items(), key=lambda
-                                           kv: (-kv[1], kv[0]))
-                        [print("{}: {}".format(k, v)) for k, v in instances]
-                    else:
-                        count_words(subreddit, word_list, instances, after,
-                                    count)
+    if after is None:
+        if len(instances) == 0:
+            print("")
+            return
+        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
+        [print("{}: {}".format(k, v)) for k, v in instances]
+    else:
+        count_words(subreddit, word_list, instances, after, count)
